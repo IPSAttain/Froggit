54fb73db0371635f32a3c97d8ee1062f2d5978fa
IPSAttain <christianwagner1@yahoo.de>
2020-09-09 23:00:33 +0200

    40

--- Froggit/module.php
+++ Froggit/module.php
@@ -53,24 +53,27 @@
 					}
 					elseif ($array[0] == 'windspeedmph')
 					{
+						$windspeed = round($array[1] * 1.609344 , 2);
 						$this->RegisterVariableFloat($array[0], $this->Translate('Wind Speed'),'~WindSpeed.kmh');
-						if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], $this->MilesToKilometer(floatval($array[1])));
+						if($this->GetValue($array[0]) != $windspeed) $this->SetValue($array[0], $windspeed);
 					}
 					elseif ($array[0] == 'maxdailygust')
 					{
+						$windspeed = round($array[1] * 1.609344 , 2);
 						$this->RegisterVariableFloat($array[0], $this->Translate('Day Wind Max'),'~WindSpeed.kmh');
-						if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], $this->MilesToKilometer(floatval($array[1])));
+						if($this->GetValue($array[0]) != $windspeed) $this->SetValue($array[0], $windspeed);
 					}
 					elseif ($array[0] == 'windgustmph')
 					{
+						$windspeed = round($array[1] * 1.609344 , 2);
 						$this->RegisterVariableFloat($array[0], $this->Translate('Wind Gust'),'~WindSpeed.kmh');
-						if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], $this->MilesToKilometer(floatval($array[1])));
+						if($this->GetValue($array[0]) != $windspeed) $this->SetValue($array[0], $windspeed);
 					}
 					elseif (substr($array[0],0,4) == 'temp' )
 					{
-						$array[1] = round($this->FahrenheitToCelsius($array[1]) ,2);
+						$temp = round(($array[1] - 32) / 1.8 ,2);
 						$this->RegisterVariableFloat($array[0], $this->Translate('Temperature') . "_(" . $array[0] . ")",'~Temperature');
-						if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], $array[1]);
+						if($this->GetValue($array[0]) != $temp) $this->SetValue($array[0], $temp);
 					}
 					elseif (substr($array[0],0,8) == 'humidity' )
 					{
@@ -79,8 +82,9 @@
 					}
 					elseif (substr($array[0],0,5) == 'barom' )
 					{
+						$pressure = round($array[1] / 0.02952998751 , 2);
 						$this->RegisterVariableFloat($array[0], $this->Translate('Air Pressure') . "_(" . $array[0] . ")",'~AirPressure.F');
-						if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], floatval($array[1] / 0.02952998751));
+						if($this->GetValue($array[0]) != $pressure) $this->SetValue($array[0], $pressure);
 					}
 					elseif (substr($array[0],-6) == 'rainin')
 					{
@@ -107,11 +111,10 @@
 					elseif ($array[0] == 'dateutc' )
 					{
 						$time = str_replace("+"," ",$array[1]);
-						//$this->SendDebug($array[0], $time , 0);
 						$this->RegisterVariableInteger($array[0], $this->Translate('Time'),'~UnixTimestamp');
 						$this->SetValue($array[0], strtotime($time));
 					}
-					else
+					/*else
 					{
 						if (isset($array[0]) && isset($array[1]))
 						{
@@ -119,83 +122,8 @@
 							if($this->GetValue($array[0]) != $array[1]) $this->SetValue($array[0], $array[1]);
 						}
 					}
+					*/
 				}
 			}
 		}
 
-
-		// Windchill Berechnung
-		private function windchill(float $temperatur, float $windspeed) {
-			if ($windspeed > 4.8) {
-				$windchill = 13.12 + 0.6215 * $temperatur - 11.37 * pow($windspeed, 0.16) + 0.3965 * $temperatur * pow($windspeed, 0.16);
-				return $windchill;
-			} else {
-				$windchill = $temperatur;
-				return $windchill;
-			}
-		}
-	
-		// Hitze-Index-Berechnung
-		private function heatindex($t, $r) {
-		// Relative Luftfeuchtigkeit limitieren
-		if ($r < 0) { $r = 0; }
-		if ($r > 100) { $r = 100; }
-	
-		// Hitzeindex
-		$hi = -8.784695 + 1.61139411*$t + 2.338549*$r - 0.14611605*$t*$r - 0.012308094*$t*$t - 0.016424828*$r*$r + 0.002211732*$t*$t*$r + 0.00072546*$t*$r*$r - 0.000003582*$t*$t*$r*$r;
-		return $hi;
-		} // end heatindex
-	
-		private function FahrenheitToCelsius(float $fahrenheit)
-		{
-			return ($fahrenheit - 32) / 1.8;
-		}
-	
-		private function MilesToKilometer(float $mph)
-		{
-			$kmh = $mph * 1.609344;
-			return $kmh;
-		}
-	
-		private function MilesToKN(float $mph)
-		{
-			$kn = $mph * 0.86897624190065;
-			return $kn;
-		}
-	
-		private function KilometerToKN(float $kmh)
-		{
-			$kn = $kmh / 1.852;
-			return $kn;
-		}
-	
-		private function MPHToMS(float $mph)
-		{
-			$ms = $mph * 0.44704;
-			return $ms;
-		}
-	
-		private function MSToMPH(float $ms)
-		{
-			$mph = $ms * 2.23694;
-			return $mph;
-		}
-	
-		private function InchToMM(float $inch)
-		{
-			$mm = $inch * 25.4;
-			return $mm;
-		}
-	
-		private function RainToInch(float $mm)
-		{
-			$inch = $mm * 0.03937007874;
-			return $inch;
-		}
-	
-		private function PressureinHGToBar($pressure)
-		{
-			$bar = $pressure * 0.02997;
-			return $bar;
-		}
-	}
