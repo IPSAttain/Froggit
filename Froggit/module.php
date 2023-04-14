b68c7da51cbe0f56d62544c9875914fe92e8f38f
IPSAttain <35669489+IPSAttain@users.noreply.github.com>
2023-04-14 10:19:54 +0200

    WH90 Battery

--- Froggit/module.php
+++ Froggit/module.php
@@ -239,6 +239,13 @@ class Froggit extends IPSModule {
 					if($ID && ($this->GetValue($key) != $batt || $SaveAllValues)) $this->SetValue($key, $batt);
 				break;
 
+				case 'wh90batt' : // DP2000
+				case 'wh80batt' :
+					$batt = $value * 75 - 150;  // from 2 == empty to 3.3 == full
+					$ID = $this->VariableCreate('integer', $key, $this->Translate('Battery Weather Station') ,'~Battery.100', 810);
+					if($ID && ($this->GetValue($key) != $batt || $SaveAllValues)) $this->SetValue($key, $batt);
+				break;
+
 				case (substr($key,0,8) == 'leakbatt') : // water leak
 					$batt = intval($value)*20; // from 0 == empty to 5 == full
 					$ID = $this->VariableCreate('integer', $key, $this->Translate('Battery') . ' ' . $this->Translate('Water Leak Sensor') . ' (' . substr($key,-1) . ')','~Battery.100', 810 + intval(substr($key,-1)));
